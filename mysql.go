d88aaa71e91dc8432ca2b69bbc0249f46cd46507
Chad Petersen <Chad.Petersen@Hexagonal.org>
2017-07-25 16:11:37 +0200

    Allows to connect to any host and port

--- mysql.go
+++ mysql.go
@@ -15,9 +15,9 @@ var (
 
 // ConnectDB enables the connection to the database and returns
 // the error if failed to establish a connection
-func ConnectDB(username, password, dbname string) {
+func ConnectDB(username, password, host, port, dbname string) {
 	var err error
-	DBCon, err = gorm.Open("mysql", fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local", username, password, dbname))
+	DBCon, err = gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", username, password, host, port, dbname))
 
 	if err != nil {
 		panic("Failed to connect to database")
